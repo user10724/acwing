242d3008ccce700381a0cb0baf31c8a919cf7f9a
user10724 <1072462276@qq.com>
2022-09-02 20:48:09 +0800

    https://github.com/user10724/acwing.git

---
+++ 整数二分/binary.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+const int N =1e6+10;
+
+int n,m;
+int q[N];
+
+
+
+int main()
+{
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<n;i++)scanf("%d",&q[i]);
+    while(m--)
+    {
+        int x;
+        scanf("%d");
+    }
+}
